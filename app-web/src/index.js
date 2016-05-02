47848e768aab7566f354b58b7beafaf704aa3a74
Oyewole Moses <mmosix@gmail.com>
2016-05-02 14:24:11 +0000

    endpoint update

--- app-web/src/index.js
+++ app-web/src/index.js
@@ -8,7 +8,7 @@ import { ActorSDK, ActorSDKDelegate } from 'actor-sdk';
 const config = {
 <<<<<<< HEAD
    endpoints: [
-     'ws://africhat.im:5443'
+     'wss://africhat.im:5443'
    ],
   // mixpanelAPIKey: 'pasteYourKeyHere',
   // bugsnagApiKey: 'pasteYourKeyHere'
